9267dc9e4752fa751cd1292990fdfabf6cb5d64f
Ahmed Gamal <agsalamh@gmail.com>
2024-06-01 20:03:15 +0300

    refactor: rename driver instance to bot

--- main.py
+++ main.py
@@ -7,22 +7,22 @@ from booking import Booking
 
 def main():
     '''Main entrypoint'''
-    with Booking(teardown=True) as booking:
-        booking.land_homepage()
+    with Booking(teardown=True) as bot:
+        bot.land_homepage()
 
         # close the window that appears when website first loads.
-        booking.explicitly_close_modal()
+        bot.explicitly_close_modal()
 
-        booking.set_currency('EGP')
+        bot.set_currency('EGP')
 
         # Double check closing the modal, it may appears after choosing the currency.
-        booking.explicitly_close_modal()
+        bot.explicitly_close_modal()
 
         # Enter destination
-        booking.where_you_going('Dahab')
+        bot.where_you_going('Dahab')
 
         # When you going ?
-        booking.when_you_going(date.today(), date(2024, 5, 30))
+        bot.when_you_going(date.today(), date(2024, 5, 30))
 
 
 if __name__ == '__main__':
